359566b3819174a2cb7b70063014dfcf2abb9b29
Aime Malaika <aimemalaika1995@gmail.com>
2022-04-17 22:22:02 +0200

    Initial repository setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import '@fontsource/lato';
+import './css/style.css';
+import logo from './images/pixels-logong.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img width="100%" src={logo} alt="logo" />
     </div>
   );
 }
