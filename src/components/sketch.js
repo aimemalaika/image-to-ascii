d1016e2e459a9fc52820072d87c414d93ab7df9e
Aime Malaika <aimemalaika1995@gmail.com>
2022-04-18 13:11:57 +0200

    Live feed convert image only

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,16 +1,36 @@
-// const skin = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
 import imageTest from './image.jpg';
 
-let image;
+const skin = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+let imageLoaded;
+
 export const setup = (p5, canvasParentRef) => {
   p5.createCanvas(400, 400).parent(canvasParentRef);
-  p5.loadImage(imageTest, (img) => {
-    image = img;
-    p5.redraw();
-  });
+  p5.noLoop();
 };
 
 export const draw = (p5) => {
-  p5.background(220);
-  p5.image(image, 0, 0, 400, 400);
+  p5.background(0);
+  p5.loadImage(imageTest, (img) => {
+    imageLoaded = img;
+    // p5.image(imageLoaded, 0, 0, 400, 400);
+    const w = p5.width / imageLoaded.width;
+    const h = p5.height / imageLoaded.height;
+    imageLoaded.loadPixels();
+    for (let i = 0; i < imageLoaded.width; i += 1) {
+      for (let j = 0; j < imageLoaded.height; j += 1) {
+        const index = (i + j * imageLoaded.width) * 4;
+        const r = imageLoaded.pixels[index];
+        const g = imageLoaded.pixels[index + 1];
+        const b = imageLoaded.pixels[index + 2];
+        const avg = (r + g + b) / 3;
+        p5.noStroke();
+        p5.fill(avg);
+        const len = skin.length;
+        const charIndex = Math.floor(p5.map(avg, 0, 255, len, 0));
+        p5.textSize(w);
+        p5.textAlign(p5.CENTER, p5.CENTER);
+        p5.text(skin.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
+      }
+    }
+  });
 };
--- src/components/videoFeed.js
+++ src/components/videoFeed.js
@@ -1,20 +1,15 @@
-import React, { PureComponent } from 'react';
 import Sketch from 'react-p5';
 import Header from './header';
 import { setup, draw } from './sketch';
 
-class videoFeed extends PureComponent {
-  render() {
-    return (
-      <>
-        <Header />
-        <div className="container">
-          <h1 className="text-white">Video Feed</h1>
-          <Sketch setup={setup} draw={draw} className="App" />
-        </div>
-      </>
-    );
-  }
-}
+const videoFeed = () => (
+  <>
+    <Header />
+    <div className="container">
+      <h1 className="text-white">Video Feed</h1>
+      <Sketch setup={setup} draw={draw} />
+    </div>
+  </>
+);
 
 export default videoFeed;
